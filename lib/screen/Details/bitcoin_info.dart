dd0c9bd700e79f8b678cc64fd66c40c14e350d0f
Mohammad Reduwan <ridwan0gts@gmail.com>
2024-07-12 02:21:07 +0800

    Crypto Wallet app

---
+++ lib/screen/Details/bitcoin_info.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BitcoinInfo extends StatelessWidget {
+  const BitcoinInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screen/Details/detail_wallet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailWalletScreen extends StatelessWidget {
+  const DetailWalletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
